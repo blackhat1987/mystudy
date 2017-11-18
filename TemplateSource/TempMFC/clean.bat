329c09b383309f3912b30c241030919ba1128793
FrogGod <jackqk@gmail.com>
2017-11-19 01:11:53 +0800

    add something

--- TemplateSource/TempMFC/clean.bat
+++ TemplateSource/TempMFC/clean.bat
@@ -5,9 +5,6 @@ del /s *.aps
 del /a:h *.suo
 rmdir /s /q .vs
 rmdir /s /q ipch
-rmdir /s /q x64
-rmdir /s /q Debug
-rmdir /s /q Release
+rmdir /s /q build
 rmdir /s /q TempMFC\x64
-rmdir /s /q TempMFC\Debug
-rmdir /s /q TempMFC\Release
+rmdir /s /q TempMFC\Win32
